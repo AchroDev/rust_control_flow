9814d09aa22c7c77421666f16f51275e53c58077
AchroDev <ztblaine@protonmail.com>
2024-04-27 22:22:10 -0500

    added comment

--- src/main.rs
+++ src/main.rs
@@ -32,6 +32,15 @@ fn example3() {
     }
 }
 
+/*
+*   When this program executes, it checks each if expression in turn and executes
+*   the first body for which the condition evaluates to true. Note that even though
+*   6 is divisible by 2, we don’t see the output number is divisible by 2, nor do we
+*   see the number is not divisible by 4, 3, or 2 text from the else block.
+*   That’s because Rust only executes the block for the first true condition,
+*   and once it finds one, it doesn’t even check the rest.
+*/
+
 // Entry point
 fn main() {
     let number = 7;
