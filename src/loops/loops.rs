36d37370225cc40bb5f5eeeabdf88f2dae5097ee
AchroDev <ztblaine@protonmail.com>
2024-04-28 00:02:39 -0500

    Added loops directory and .rs file with example

---
+++ src/loops/loops.rs
@@ -0,0 +1,31 @@
+/*
+*   Repitition with Loops
+*/
+
+/*
+*   It’s often useful to execute a block of code more than once. For this task,
+*   Rust provides several loops, which will run through the code inside the loop
+*   body to the end and then start immediately back at the beginning.
+*   Rust has three kinds of loops: loop, while, and for. Let’s try each one.
+*/
+
+fn loop_one() {
+    loop {
+        println!("again!");
+        break;
+    }
+}
+
+/*
+*   When we run this program, we’ll see again! printed over and over continuously until
+*   we stop the program manually. Most terminals support the keyboard shortcut ctrl-c to
+*   interrupt a program that is stuck in a continual loop.
+*/
+
+// Fortunately, Rust also provides a way to break out of a loop using code.
+// You can place the break keyword within the loop to tell the program when to stop executing the loop.
+
+// Entry point
+fn main() {
+    loop_one();
+}
