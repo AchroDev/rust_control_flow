64d1818c42a13fdc61068c4f1ca64f0fceb5b351
AchroDev <ztblaine@protonmail.com>
2024-04-27 21:32:03 -0500

    Updated main.rs to example if expression

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+/*
+*   An if expression allows you to branch your code depending on conditions.
+*   You provide a condition and then state, “If this condition is met, run
+*   this block of code. If the condition is not met, do not run this block of code.”
+*/
+
 fn main() {
-    println!("Hello, world!");
+    let number = 3;
+
+    if number < 5 {
+        println! {"condition was true"};
+    } else {
+        println! {"condition was false"};
+    }
 }
